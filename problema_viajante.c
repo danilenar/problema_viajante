310e5c04741c2c8b6c6b3f32bfa90e4e62d0b39b
Castor <daniellenarduzzi@gmail.com>
2018-04-13 01:02:25 -0300

    ingreso de numero de vertices y aristas

---
+++ problema_viajante.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(int argc, char const *argv[]) {
+  /* code */
+  int v = 0;
+  printf("ingrese numero de vertices:   ");
+  scanf("%d\n", &v);
+  printf("%d\n", v);
+  return 0;
+}
