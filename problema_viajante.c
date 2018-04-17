67bec9e92f5d86f1ce64b598dc1581662c3cdfdc
Castor <daniellenarduzzi@gmail.com>
2018-04-17 00:13:05 -0300

    terminado el ingreso de datos

--- problema_viajante.c
+++ problema_viajante.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 int main() {
-
   /*ingreso numero de vertices*/
   int v = 0;
   printf("ingrese numero de vertices: ");
@@ -12,29 +11,59 @@ int main() {
   scanf("%d", &e);
 
 
-  int matriz[e];
+  int matriz[e][v];
+  int pesos[e];
   for (int i = 0; i < e ; i++) {
-    int matriz[i][e];
     for (int j = 0; j < v; j++) {
       printf("v%de%d: ", i+1, j+1);
       scanf("%d", &matriz[i][j]);
     }
+    printf("P(e%d): ", i+1);
+    scanf("%d", &pesos[i]);
   }
-
-  // encabezado de la matriz
-  printf("    ");
-  for (int i = 1; i <= v ; i++) {
-    printf("v%d ", i);
+  //
+  // // encabezado de la matriz
+  // //esquina superior izquierda
+  // printf("\n\u2554");
+  // //bordes superiores
+  // for (int i = 0; i < v; i++) {
+  //   //primer borde, doble linea
+  //   if (i=0) {
+  //     printf("\u2550\u2550\u2550\u2566");
+  //   }
+  //   //el resto son de linea simple
+  //   else{
+  //     printf("\u2550\u2550\u2550\u2564");
+  //   }
+  // }
+  // //esquina superior derecha
+  // printf("\u2550\u2550\u2550\u2556\n");
+  // //celda vacia
+  // printf("\u2551   \u2551");
+  // //encabezados con nombres de vertices
+  // for (int i = 0; i < v ; i++) {
+  //   if (i<v-1) {
+  //     printf("v%d \u2502", i+1);
+  //   }
+  //   else {
+  //     printf("v%d \u2551", i+1);
+  //   }
+  // }
+  //
+  printf("\n   ");
+  for (int i = 0; i < v ; i++) {
+      printf("v%d ", i+1);
   }
-  printf("\n");
+  printf("P()");
 
   for (int i = 0; i < e; i++) {
-    printf("e%d  ", i+1);
-    for (int j = 0; j < v; j++) {
-      printf("%d", matriz[i][j]);
-    }
     printf("\n");
+    printf("e%d ", i+1);
+    for (int j = 0; j < v; j++) {
+      printf("%d  ", matriz[i][j]);
+    }
+    printf("%d", pesos[i]);
   }
-
+  printf("\n");
   return 0;
 }
